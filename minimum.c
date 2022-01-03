67092cd95d30970741a6d55cdff3947719b76804
Yusuf <kamaldeen32@gmail.com>
2022-01-04 00:13:32 +0100

    recursion for factorial in c

---
+++ minimum.c
@@ -0,0 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+float minimum(int s, float array[]);
+
+int main (void)
+
+
+{
+
+
+    float k[13] = {10, 20, 30, 5, 100 , 0.9, 4, 6, 7, 200, 2, 5, 2000};
+    int y = 13;
+
+      printf("minimum %f \n", minimum (y, k));
+
+}
+
+
+float minimum (int n, float s[])
+    {
+        float min = s[1];
+        for (int i=0;  i < n; i++)
+        {
+            if (s[i] < min)
+            {
+                min = s[i];
+            }
+
+
+        }
+        return min;
+    }
